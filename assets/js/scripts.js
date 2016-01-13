bb033c32d5b16c8a41810c2aa86da2a5272ad8ff
crazyyy <crazyyy@gmail.com>
2016-01-13 02:33:17 +0200

    work with header

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,3 +17,35 @@
 }());
 
 // Place any jQuery/helper plugins in here.
+
+// get browser width and height on window resize
+var headerEl = document.getElementById('header');
+// when window size changed - resize first block
+window.addEventListener('resize', setWindowSize);
+function setWindowSize() {
+  if (typeof (window.innerWidth) == 'number') {
+    myWidth = window.innerWidth;
+    myHeight = window.innerHeight;
+    setheight(myHeight);
+  } else {
+    if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight)) {
+      myWidth = document.documentElement.clientWidth;
+      myHeight = document.documentElement.clientHeight;
+      setheight(myHeight);
+    } else {
+      if (document.body && (document.body.clientWidth || document.body.clientHeight)) {
+        myWidth = document.body.clientWidth;
+        myHeight = document.body.clientHeight;
+        setheight(myHeight);
+      }
+    }
+  }
+}
+function setheight(height) {
+  headerEl.style.height = height + 'px';
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  setWindowSize();
+});
+
