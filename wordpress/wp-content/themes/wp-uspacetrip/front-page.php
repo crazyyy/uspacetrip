fa6259986b95ce072743d57448f41338cccf9f82
crazyyy <crazyyy@gmail.com>
2016-01-18 01:20:30 +0200

    before wp upd

---
+++ wordpress/wp-content/themes/wp-uspacetrip/front-page.php
@@ -0,0 +1,265 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> </title>
+
+    <!-- icons -->
+    <link href="favicon.ico" rel="shortcut icon">
+
+    <!-- css + javascript -->
+    <link rel="stylesheet" href="style.css" media="all">
+
+    <!--[if lt IE 9]>
+      <script type="text/javascript" src="js/html5shiv.js"></script>
+      <script type="text/javascript" src="js/selectivizr.js"></script>
+      <script type="text/javascript" src="js/respond.js"></script>
+    <![endif]-->
+    <script type="text/javascript" src="///cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+</head>
+<body dasa>
+<!-- wrapper -->
+<div class="wrapper">
+  <header id="header" role="banner" onload="setWindowSize()">
+
+    <div class="header-menu">
+
+      <div class="logo">
+        <img src="img/logo.png" alt="" title="">
+      </div><!-- /logo -->
+
+      <div class="social">
+        <li><a href="#"><i class="fa fa-facebook-official"></i></a></li>
+        <li><a href="#"><i class="fa fa-youtube-square"></i></a></li>
+        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+      </div>
+
+      <nav class="nav" role="navigation">
+        <ul class="headnav">
+        <li><a href="#">About</a></li>
+        <li><a href="#">How it Looks</a></li>
+        <li><a href="#">How to Use It</a></li>
+        <li><a href="#">How We Do It</a></li>
+        <li><a href="#">Reviews</a></li>
+        <li><a href="#">Tech</a></li>
+        <li><a href="#">Contact Us</a></li>
+        </ul>
+      </nav><!-- /nav -->
+
+      <a href="#" class="btn btn-blue btn-head-order">Place your order</a>
+    </div><!-- /.header-menu -->
+
+    <div class="stars"></div>
+    <div class="earth"></div>
+    <div class="twinkling"></div>
+
+    <div class="inner">
+      <div class="owl-carousel">
+        <div class="item">
+          <h6>We know how to <span>get high</span></h6>
+          <p>We launch dreams into stratosphere and create beautiful videos to memorise this moment</p>
+          <span class="container-img">
+            <img src="img/slider-3.png" alt="">
+          </span>
+        </div>
+      </div><!-- owl-carousel -->
+      <a href="#" class="btn btn-white btn-head-recall">Сall back</a>
+    </div><!-- /.inner -->
+  </header><!-- /header -->
+
+  <section role="main">
+
+    <article id="about" class="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h5>uspacetrip</h5>
+            <p>If you are dreaming to make a trip above the stars - we can make your goal a little closer!  Uspacetrip launch<br>your toy copy to the stratosphere! In the issue you’ll have a short video-story of your incrediable<br>flight accompanied with your favorite song.</p>
+            <p>If you are looking for an impressive gift for yourself or your friends - Uspacetrip is just what you need!</p>
+            <p>We know how to get high!</p>
+          </div>
+        </div><!-- row -->
+        <div class="row">
+          <div id="how-it-looks" class="how-it-looks col-md-12">
+            <h4><span>how it looks</span></h4>
+            <span class="iframe"></span>
+          </div><!-- /#how-it-looks.how-it-looks -->
+        </div><!-- /.row -->
+      </div><!-- container -->
+    </article><!-- about -->
+
+    <article id="how-to-use-it" class="how-to-use-it">
+      <div class="container">
+        <div class="row">
+          <h4 class="col-md-12">How to use it</h4>
+
+          <div class="row tabs-header" id="tabs">
+            <div class="col-md-2">
+              <a href="#birthday" class="tab-link active"><span>Birthday</span></a>
+            </div>
+            <div class="col-md-2">
+              <a href="#wedding" class="tab-link"><span>Wedding</span></a>
+            </div>
+            <div class="col-md-3">
+              <a href="#viral-branding" class="tab-link"><span>Viral branding</span></a>
+            </div>
+            <div class="col-md-2">
+              <a href="#others" class="tab-link"><span>Others</span></a>
+            </div>
+          </div><!-- row tabs-header -->
+
+          <div class="row tabs-container">
+            <div class="tabContent col-md-12" id="birthday">
+              <img src="img/content-birthday.jpg" alt="">
+              <h5>Birthday</h5>
+              <p>It’s so hard to surprise someone nowadays. If you want to present memorable gift and new emotions in unussual way - Uspacetrip can help you for sure. We can add something really new and extraordinary to make your gift standing out from all.</p>
+              <a href="#" class="btn btn-blue">Place your order</a>
+            </div><!-- tabContent col-md-12 birthday -->
+            <div class="tabContent col-md-12" id="wedding">
+            </div><!-- wedding -->
+            <div class="tabContent col-md-12" id="viral-branding">
+            </div><!-- viral-branding -->
+            <div class="tabContent col-md-12" id="others">
+            </div><!-- others -->
+          </div>
+
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </article><!-- /#how-to-use-it.how-to-use-it -->
+
+
+    <article class="how-we-do-it" id="how-we-do-it">
+      <div class="container">
+        <div class="row">
+          <h4 class="col-md-12">how we do it</h4>
+          <ul class="col-md-12">
+            <li>
+              <img src="img/how-we-do-it-1.png" alt="">
+              <h5>We take your photo</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </li>
+            <li>
+              <img src="img/how-we-do-it-2.png" alt="">
+              <h5>We make 3D model</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </li>
+            <li>
+              <img src="img/how-we-do-it-3.png" alt="">
+              <h5>We send it into stratosphere</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </li>
+            <li>
+              <img src="img/how-we-do-it-4.png" alt="">
+              <h5>We create a video and send it to you</h5>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </li>
+          </ul>
+          <h6>Do you have any questions?</h6>
+          <a href="" class="btn btn-white btn-place-order">Place your order</a>
+        </div><!-- row -->
+      </div><!-- /.container -->
+    </article><!-- /#how-we-do-it.how-we-do-it -->
+
+
+    <article class="what-are-people-saying" id="what-are-people-saying">
+      <div class="container">
+        <div class="row">
+          <h4 class="col-md-12">What are people saying about us</h4>
+          <div class="col-md-4 what-are-people-block">
+            <p>You should have seen eyes of our boss when video started to play! At that moment our team realized that was exactly the present he want. Thanks Uspasetrip for possibility to amaze with a gift even a person who is so hard to please.</p>
+            <span class="author">Nikol</span>
+          </div><!-- what-are-people-block -->
+          <div class="col-md-4 what-are-people-block">
+            <p>Thanks for a good work, guys! Cool video, excelent shooting, nice team and great idea of such service for sure.</p>
+            <span class="author">Brian R.</span>
+          </div><!-- what-are-people-block -->
+          <div class="col-md-4 what-are-people-block">
+            <p>I am a member of a small team of IT professionals. Recently we launched an e-commerce start-up and we believe our product is a game changer! The statuette of our CEO proudly holding our logo while flying in the stratosphere was a bright beginning of our project! Thank you, Uspasetrip!</p>
+            <span class="author">Alberto, Madrid.</span>
+          </div><!-- what-are-people-block -->
+          <div class="col-md-4 what-are-people-block">
+            <p>This team has made a video for our company and coped with this task perfectly! Тhere were small offsets on terms, but such result worth the wait. Good quality and sociable team is a recipe for success.</p>
+            <span class="author">Vladislav</span>
+          </div><!-- what-are-people-block -->
+          <div class="col-md-4 what-are-people-block">
+            <p>It was my parents 30th wedding anniversary, I looked for a present for a couple of weeks but everything seemed so trivial, then I came across Uspacetrip! The video was absolutely mind blowing, I couldn't hope to find a better present! Thank you guys! Keep up the great work!</p>
+            <span class="author">John, London</span>
+          </div><!-- what-are-people-block -->
+          <div class="col-md-4 what-are-people-block">
+            <p>We decided to present such impression to our friends for their merriage. We translate video on a big screen when all guests were present. The reaction was amazing! Everyone liked, but the main thing that the newlyweds were happy too. Uspacetrip, don’t stop helping impress :)</p>
+            <span class="author">Kira & Steve</span>
+          </div><!-- what-are-people-block -->
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </article><!-- /#what-are-people-saying.what-are-people-saying -->
+
+    <article class="technical-specifications" id="technical-specifications">
+      <div class="container">
+        <div class="row">
+          <h4 class="col-md-12">technical specifications</h4>
+          <div class="technical-specifications-content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div><!-- /.technical-specifications-content -->
+        </div><!-- row -->
+      </div><!-- /.container -->
+    </article><!-- /#technical-specifications.technical-specifications -->
+
+    <article class="find-uspacetrip">
+      <div class="container">
+        <div class="row">
+          <h3 class="col-md-12">
+            Find Out More About Stratospher Launch.<br><a href="#">#uspacetrip</a> community <a href="#"><i class="fa fa-facebook-official"></i></a> <a href="#"><i class="fa fa-youtube-square"></i></a> <a href="#"><i class="fa fa-instagram"></i></a>
+          </h3>
+        </div><!-- row -->
+      </div><!-- /.container -->
+    </article><!-- /.find-uspacetrip -->
+
+    <article class="contact-us" id="contact-us">
+      <div class="container">
+        <div class="row">
+          <h4>Contact Us</h4>
+          <form action="">
+            <input type="text" name="name" placeholder="Your name">
+            <input type="text" name="mail" placeholder="E-mail">
+            <input type="text" name="phone" placeholder="Mobile">
+            <input type="text" name="messege" placeholder="Your message">
+            <input type="submit" value="Send messege">
+          </form>
+        </div><!-- /.row -->
+      </div><!-- /.container -->
+    </article><!-- /#contact-us.contact-us -->
+
+  </section><!-- /section -->
+</div><!-- /wrapper -->
+
+<footer role="contentinfo">
+  <div class="inner">
+    <div class="copyright">
+      <ul>
+        <li>uspacetrip</li>
+        <li><a href="mailto:hello@uspacetrip.com">hello@uspacetrip.com</a></li>
+      </ul>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id.</p>
+    </div><!-- /copyright -->
+
+    <div class="paymathods">
+      <p>Secure payments with 100% money back guarantee</p>
+    </div><!-- /.paymathods -->
+
+    <div class="maillist">
+      <a class="btn order-free">
+        Order Free Stratosphere<br>Launch Guide <span>By Mail</span>
+      </a><!-- /.order-free -->
+      <p>© 2015 uspacetrip. All Rights Reserved.</p>
+    </div><!-- /.maillist -->
+
+  </div><!-- /.inner -->
+</footer><!-- /footer -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+  <script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
