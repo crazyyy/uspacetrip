5d92935aaee12d6fa82b0c32f31c6b918a83585c
crazyyy <crazyyy@gmail.com>
2016-01-24 00:45:38 +0200

    slider fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1641,10 +1641,14 @@ $(document).ready(function() {
 
   $(".owl-carousel").owlCarousel({
 
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true
+      navigation: true, // Show next and prev buttons
+      slideSpeed: 300,
+      paginationSpeed: 400,
+      singleItem: true,
+      autoplay: true,
+      navigation: true,
+      navigationText: ['<i class="fa fa-chevron-left"></i>','<i class="fa fa-chevron-right"></i>'],
+      pagination: false
 
       // "singleItem:true" is a shortcut for:
       // items : 1,
