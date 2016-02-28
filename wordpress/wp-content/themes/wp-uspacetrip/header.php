789da4cce913d8a49d047f525cad5a9142593ea4
crazyyy <crazyyy@gmail.com>
2016-02-28 18:29:41 +0200

    bugnotfixed

--- wordpress/wp-content/themes/wp-uspacetrip/header.php
+++ wordpress/wp-content/themes/wp-uspacetrip/header.php
@@ -3,12 +3,9 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1">
 
   <title><?php wp_title( '' ); ?><?php if ( wp_title( '', false ) ) { echo ' :'; } ?> <?php bloginfo( 'name' ); ?></title>
-
-  <link href="http://www.google-analytics.com/" rel="dns-prefetch"><!-- dns prefetch -->
-  <!-- meta -->
 
   <!-- icons -->
   <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
